0d4043f559b99605886c0aa6646c0b7173b7eed8
julianodecarlo <julianodecarlo@gmail.com>
2026-08-31 17:56:55 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'fuel-tracker-v2';
+const ASSETS_TO_CACHE = [
+  '/fuel-tracker/',
+  '/fuel-tracker/index.html',
+  '/fuel-tracker/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
